49e9f9ffb0f194a5e7b66e433276fe276fca5c92
Fan Xie <fxie46@gmail.com>
2025-04-03 16:40:07 +0800

    modify

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ torch::Tensor cute_compute_kv_F16F16F16F16(torch::Tensor k, torch::Tensor v);
 torch::Tensor cute_compute_kv_F32F16F16F32(torch::Tensor k, torch::Tensor v);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  m.def("cute_compute_kv_F16F16F16F16", torch::wrap_pybind_function(cute_compute_kv_F16F16F16F16), "cute_compute_kv_F16F16F16F16");
   m.def("cute_compute_kv_F32F16F16F32", torch::wrap_pybind_function(cute_compute_kv_F32F16F16F32), "cute_compute_kv_F32F16F16F32");
 }
