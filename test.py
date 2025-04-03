bb548918030bd470c62b6013f83086fbca64ab1d
Fan Xie <fxie46@gmail.com>
2025-04-03 16:17:25 +0800

    modify

--- test.py
+++ test.py
@@ -76,7 +76,7 @@ def test_kv_match_cute_f16_torch_f16(k, v, myflash):
     print("✅ kv results match")
 
 
-def test_kv_match_F32F16F16F32(k, v, myflash):
+def test_kv_match_cute_F32F16F16F32_torch_f16(k, v, myflash):
     assert k.dtype == torch.float16
     assert v.dtype == torch.float16
     BLOCK = 64
@@ -124,6 +124,57 @@ def test_kv_match_F32F16F16F32(k, v, myflash):
         print(f"✅ block : {i}, kv results match")
 
     print("✅ kv results match")
+
+
+def test_kv_match_cute_F32F16F16F32_torch_f32(k, v, myflash):
+    assert k.dtype == torch.float16
+    assert v.dtype == torch.float16
+    BLOCK = 64
+    B, H, N, d = k.shape
+    num_block = (N + BLOCK - 1) // BLOCK
+
+    def torch_compute_kv_all_f32():
+
+        kv = torch.zeros(d, d).to(torch.float32).to(q.device)
+        kv_output = torch.zeros(num_block, d, d).to(torch.float32).to(q.device)
+
+        for i in range(num_block):
+            si = i * BLOCK
+            ei = min(si + BLOCK, N)
+            ki = k[:, :, si:ei].contiguous().to(torch.float32)
+            vi = v[:, :, si:ei].contiguous().to(torch.float32)
+
+            new_kv = torch.matmul(ki.transpose(-1, -2), vi).to(torch.float32)
+            kv = kv + new_kv
+            kv_output[i] = kv.detach().clone()
+            # print(f"data types. ki : {ki.dtype}, vi : {vi.dtype},  new_kv: {new_kv.dtype}, kv: {kv.dtype}")
+        return kv_output
+
+    torch_kv_output = torch_compute_kv_all_f32()
+    cute_kv_output = myflash.cute_compute_kv_F32F16F16F32(k, v)
+
+    assert torch_kv_output.dtype == torch.float32
+    assert cute_kv_output.dtype == torch.float32
+
+    print(f"test_kv_match. num_block : {num_block}")
+
+    for i in range(num_block):
+        print(f"torch_kv_output shape: {torch_kv_output[i].shape}")
+        print(f"cute_kv_output shape: {cute_kv_output[i].shape}")
+
+        print(f"block: {i}, torch_kv_output: {torch_kv_output[i]}, cute_kv_output: {cute_kv_output[i]}")
+
+        torch.testing.assert_close(
+            torch_kv_output[i],
+            cute_kv_output[i],
+            rtol=1e-3,
+            atol=1e-5,
+        )
+
+        print(f"✅ block : {i}, kv results match")
+
+    print("✅ kv results match")
+
 
 if __name__ == "__main__":
 
@@ -180,4 +231,8 @@ if __name__ == "__main__":
     # test_kv_match_cute_f16_torch_f16(k1, v1, myflash)
 
     # case2 cute F32F16F16F32, torch F16 matmul, then convert to F32 to accumulate，not match!
-    test_kv_match_F32F16F16F32(k1, v1, myflash)
+    test_kv_match_cute_F32F16F16F32_torch_f16(k1, v1, myflash)
+
+
+    # case3 cute F32F16F16F32, torch all f32, match
+    # test_kv_match_cute_F32F16F16F32_torch_f32(k1, v1, myflash)
