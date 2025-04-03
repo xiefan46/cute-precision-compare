09d13cb24df0ca05cc0f0a63b4a9a06af88444d6
Fan Xie <fxie46@gmail.com>
2025-04-03 22:25:03 +0800

    modify

---
+++ compare_gemm/cute_precision_f16.cu
@@ -0,0 +1,136 @@
+#include <cuda.h>
+#include <cuda_fp16.h>
+#include <cuda_runtime.h>
+#include <torch/types.h>
+
+#include <cute/tensor.hpp>
+#include <cutlass/array.h>
+#include <cutlass/cutlass.h>
+#include <cutlass/numeric_conversion.h>
+#include <cutlass/numeric_types.h>
+#include <tuple>
+
+using namespace cute;
+
+#define PRINT(name, content) \
+    print(name);             \
+    print(" : ");            \
+    print(content);          \
+    print("\n");
+
+#define PRINT_TENSOR(name, content) \
+    print(name);             \
+    print(" : ");            \
+    print_tensor(content);          \
+    print("\n");
+
+
+namespace config {
+using namespace cute;
+
+template <typename T_, int kHeadDim_ = 64, int BLOCK_ = 64>
+struct FlashConfig {
+  using T = T_;
+  static constexpr int kHeadDim = kHeadDim_;
+  static constexpr int BLOCK = BLOCK_;
+
+  using mma_op = SM80_16x8x16_F16F16F16F16_TN;
+  using mma_traits = MMA_Traits<mma_op>;
+  using mma_atom = MMA_Atom<mma_traits>;
+  static constexpr int kMmaEURepeatM = 1; // 4 -> 1
+  static constexpr int kMmaEURepeatN = 1;
+  static constexpr int kMmaEURepeatK = 1;
+
+  using mma_atom_shape = mma_traits::Shape_MNK;
+  static constexpr int kMmaPM = 1 * kMmaEURepeatM * get<0>(mma_atom_shape{});
+  static constexpr int kMmaPN = 1 * kMmaEURepeatN * get<1>(mma_atom_shape{}); // 2 -> 1
+  static constexpr int kMmaPK = 1 * kMmaEURepeatK * get<2>(mma_atom_shape{});
+
+  using MMA_EU_RepeatT = decltype(make_layout(make_shape(
+      Int<kMmaEURepeatM>{}, Int<kMmaEURepeatN>{}, Int<kMmaEURepeatK>{})));
+  using MMA_P_T = Tile<Int<kMmaPM>, Int<kMmaPN>, Int<kMmaPK>>;
+
+  using TiledMMA =
+      decltype(make_tiled_mma(mma_atom{}, MMA_EU_RepeatT{}, MMA_P_T{}));
+  static constexpr int kThreadNum = size(TiledMMA{});
+
+};
+
+}  // namespace config
+
+
+
+
+template <typename config>
+__global__ void gemm_kernel(const half_t* k, const half_t* v, half_t* kv_out, const int N)
+{
+    using namespace cute;
+    using TiledMMA = typename config::TiledMMA;
+
+
+    constexpr int BLOCK = config::BLOCK;
+    constexpr int kHeadDim = config::kHeadDim;
+
+    const int bx = blockIdx.x;
+    const int tx = threadIdx.x;
+
+    Tensor Kt = make_tensor(make_gmem_ptr<half_t>(k), make_shape(Int<kHeadDim>{}, N), make_stride(Int<1>{}, Int<kHeadDim>{})); // d x N
+    Tensor Vt = make_tensor(make_gmem_ptr<half_t>(v), make_shape(Int<kHeadDim>{}, N), make_stride(Int<1>{}, Int<kHeadDim>{})); // d x N
+    Tensor gKV = make_tensor(make_gmem_ptr<half_t>(kv_out),
+                             make_shape(Int<kHeadDim>{}, Int<kHeadDim>{}), make_stride(Int<kHeadDim>{}, Int<1>{})); // d x d
+
+    TiledMMA mma;
+    ThrMMA thr_mma = mma.get_slice(tx);
+
+    if (thread0()) {
+        PRINT("mma size", size(mma));
+        PRINT("num_block", num_block);
+    }
+
+
+    Tensor gKt = local_tile(Kt, make_tile(Int<kHeadDim>{}, Int<BLOCK>{}), make_coord(0, block_id)); // d x BLOCK
+    Tensor gVt = local_tile(Vt, make_tile(Int<kHeadDim>{}, Int<BLOCK>{}), make_coord(0, block_id)); //d x BLOCK
+
+    Tensor tAgKt = thr_mma.partition_A(gKt);
+    Tensor tArKt = thr_mma.partition_fragment_A(gKt);
+    Tensor tBgVt = thr_mma.partition_B(gVt);
+    Tensor tBrVt = thr_mma.partition_fragment_B(gVt);
+
+    cute::copy(tAgKt, tArKt);
+    cute::copy(tBgVt, tBrVt);
+
+    Tensor tCgKV = thr_mma.partition_C(gKV);
+    Tensor tCrKV = thr_mma.partition_fragment_C(gKV);
+    clear(tCrKV);
+
+    __syncthreads();
+    cute::gemm(mma, tArKt, tBrVt, tCrKV);
+    __syncthreads();
+    cute::copy(tCrKV, tCgKV);
+}
+
+
+torch::Tensor cute_gemm(torch::Tensor k, torch::Tensor v) {
+    int N = k.size(0);
+    int d = k.size(1);
+
+    int BLOCK = 64;
+    int num_block = (N + BLOCK - 1) / BLOCK;
+
+    PRINT("num_block", num_block);
+
+    auto kv_out = torch::zeros({d, d}, torch::TensorOptions().dtype(torch::kFloat16).device(torch::Device(torch::kCUDA, 0)));
+
+    // only for head_dim=64
+    config::FlashConfig<cute::half_t> config;
+    dim3 block = config.kThreadNum;
+    dim3 grid(1);
+    auto kernel = gemm_kernel<decltype(config)>;
+    PRINT("grid", grid);
+    PRINT("block", block);
+
+    kernel<<<grid, block>>>((cute::half_t*) k.data_ptr(), (cute::half_t*)v.data_ptr(), (cute::half_t*)kv_out.data_ptr(),  N);
+    cudaDeviceSynchronize();
+
+    return kv_out;
+}
---
+++ compare_gemm/test.py
@@ -0,0 +1,82 @@
+import os
+import torch
+from torch.utils.cpp_extension import load
+import random
+import numpy as np
+from torch.cuda.amp import autocast, GradScaler
+
+
+def set_seed(seed=42):
+    random.seed(seed)
+
+    # NumPy
+    np.random.seed(seed)
+
+    # PyTorch CPU
+    torch.manual_seed(seed)
+
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
+
+
+if __name__ == "__main__":
+
+    os.environ['TORCH_CUDA_ARCH_LIST'] = '8.0'
+
+    REMOVE_NVCC_FLAGS = [
+        "-D__CUDA_NO_HALF_OPERATORS__",
+        "-D__CUDA_NO_HALF_CONVERSIONS__",
+        "-D__CUDA_NO_BFLOAT16_CONVERSIONS__",
+        "-D__CUDA_NO_HALF2_OPERATORS__",
+    ]
+    for flag in REMOVE_NVCC_FLAGS:
+        try:
+            torch.utils.cpp_extension.COMMON_NVCC_FLAGS.remove(flag)
+        except ValueError:
+            pass
+
+
+    torch.manual_seed(0)
+    # Load the CUDA kernel as a python module
+    myflash = load(name='myflash',
+                   sources=[
+                       'main.cpp',
+                       'cute_precision_f16.cu',
+                   ],
+                   extra_cuda_cflags=[
+                       '-O2',
+                       '-lcublas',
+                       '-lcublasLt',
+                       '-std=c++17',
+                       '-I/root/cutlass/include',
+                       '-I/root/cutlass/tools/util/include',
+                   ],
+                   )
+
+
+    set_seed(10086)
+
+    N = 512
+    # NOTE: we only support d = 64!
+    d = 64
+
+    k = torch.randn(N, d).cuda().half()
+    v = torch.randn(N, d).cuda().half()
+
+    torch_kv = torch.matmul(k.transpose(-1, -2), v)
+
+    cute_kv = myflash.cute_gemm(k, v)
+
+    torch.testing.assert_close(
+        torch_kv,
+        cute_kv,
+        rtol=1e-3,
+        atol=1e-5,
+    )
